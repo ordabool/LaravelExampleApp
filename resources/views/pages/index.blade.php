d0ce18c6894e3145b8c381d3f9d04867249862f1
Or Dabool <O.Dabool@avatrade.com>
2018-02-04 15:28:49 +0200

    added pages and basic routes + template handling with Blade

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor magni nisi dolorem deserunt debitis laborum corrupti, reprehenderit ducimus temporibus non. Consequuntur provident praesentium distinctio repellat consequatur eaque at repellendus enim?</p>
+@endsection
